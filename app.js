231408813c3fc55366a5fe7218d513d2f73cc94d
Yogesh Namdev <namdev.yash10@gmail.com>
2024-07-13 13:52:53 +0530

    fix header

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.set('io', io)
 app.use((req, res, next) => {
   res.header(
     'Access-Control-Allow-Origin',
-    'https://chattu-frontend-ten.vercel.app/'
+    'https://chattu-frontend-ten.vercel.app'
   ) // You can specify your allowed origin instead of *
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
   res.header(
