f3efd90be822c2f82f4851a3fe1e574427f27256
Yogesh Namdev <namdev.yash10@gmail.com>
2024-07-13 11:17:25 +0530

    config.js add origin

--- constants/config.js
+++ constants/config.js
@@ -1,13 +1,13 @@
 const corsOptions = {
   origin: [
-    "http://localhost:5173",
-    "http://localhost:4173",
-    process.env.CLIENT_URL,
+    'http://localhost:5173',
+    'http://localhost:4173',
+    'https://chattu-frontend-azure.vercel.app/',
   ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
-};
+}
 
-const CHATTU_TOKEN = "chattu-token";
+const CHATTU_TOKEN = 'chattu-token'
 
-export { corsOptions, CHATTU_TOKEN };
+export { corsOptions, CHATTU_TOKEN }
