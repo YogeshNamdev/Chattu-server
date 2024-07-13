7a84c0b7c70f6c60ac0acc5a6b5975dda4ccb980
Yogesh Namdev <namdev.yash10@gmail.com>
2024-07-13 12:44:06 +0530

    modify config file

--- constants/config.js
+++ constants/config.js
@@ -2,7 +2,7 @@ const corsOptions = {
   origin: [
     'http://localhost:5173',
     'http://localhost:4173',
-    'https://chattu-frontend-ten.vercel.app/',
+    process.env.CLIENT_URL,
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
